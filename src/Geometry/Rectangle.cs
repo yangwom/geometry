523423bf6a1d41094693a987c239238dac190a7d
yang vieira <yangflix3@gmail.com>
2022-08-16 09:26:13 -0400

    class Rectangle

--- src/Geometry/Rectangle.cs
+++ src/Geometry/Rectangle.cs
@@ -1,4 +1,32 @@
-﻿namespace Geometry;
+﻿using static System.Console;
+namespace Geometry;
 public class Rectangle
-{
+{    
+
+      double  _width;
+
+      double _height;
+    
+     static void Main()
+     {
+        // teste
+       var instance = new Rectangle(-5, -2);
+        WriteLine(instance.Area);
+     }
+    public double Width { get { return _width; } set {
+        if(value < 0) throw new ArgumentException("All sides must be greater than zero");
+        _width = value;
+    }}
+    public double Height { get { return _height; } set {
+        if(value < 0) throw new ArgumentException("All sides must be greater than zero");
+        _height = value;
+    } }
+    public double Area { get { return _width * _height; }}
+
+    public double Perimeter { get { return Width * Height; } }
+
+    public Rectangle(double width, double height) {
+    Width = width;
+    Height = height;
+    }
 }
